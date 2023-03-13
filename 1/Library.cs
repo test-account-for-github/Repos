a10008946932b97b61100007682a869ae6533427
ANKtwo <anktwo.7234@gmail.com>
2023-03-13 15:13:14 +0300

    new project file

---
+++ 1/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    internal class Library
+    {
+        private string Name { get; set; }
+        private string Adress { get; set; }
+        private string OpenAge { get; set; }
+    }
+}
